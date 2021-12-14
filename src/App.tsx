7ff1c5fda3f0e2974bddad3ef27020fd7603a7e6
Vaintti <Vaintti@users.noreply.github.com>
2021-12-14 02:37:35 +0200

    Add backgrounds for different weathers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Api from './api';
 import TemperatureComponent from './components/TemperatureComponent';
@@ -6,22 +6,37 @@ import { CircleLoader } from 'react-spinners';
 import WeatherDescriptionComponent from './components/WeatherDescriptionComponent';
 
 function App() {
-  Api.getWeather('heidenheim')
-  .then(data => setWeather(data))
-  .catch(err => console.log(err));
-
+  // State
   const [weather, setWeather] = React.useState<Weather | null>(null);
 
-  if (weather === null) {
-    return <div className="loader"><CircleLoader color="white" size={100}/></div>;
+  // Functions
+  const updateWeather = async () => {
+    Api.getWeather('heidenheim')
+      .then(data => setWeather(data))
+      .catch(err => console.log(err));
+  };
+
+  const getBackgroundImageForWeather = (weather: Weather) => {
+    if (['Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash', 'Squall'].includes(weather.category)) return 'weather-type-backgrounds/Mist.jpg';
+    return `weather-type-backgrounds/${weather.category}.jpg`;
   }
 
+  // Effects
+  useEffect(() => {
+    updateWeather();
+  });
+
+  // Loading state
+  if (weather === null) {
+    return <div className="loader"><CircleLoader color="white" size={100}/></div>;
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{background: `linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${getBackgroundImageForWeather(weather)})`}}>
       <TemperatureComponent weather={weather}/>
       <WeatherDescriptionComponent weather={weather}/>
     </div>
   );
-}
+};
 
 export default App;
